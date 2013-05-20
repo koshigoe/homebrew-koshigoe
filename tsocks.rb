65655afe94c03753f7101903126b7a96f91db828
koshigoe <koshigoeb@gmail.com>
2013-05-20 16:40:56 +0900

    add tsocks

---
+++ tsocks.rb
@@ -0,0 +1,36 @@
+
+require 'formula'
+
+# via: https://trac.macports.org/browser/trunk/dports/net/tsocks
+#
+# === #{etc}/tsocks.conf
+# server = 127.0.0.1
+# server_type = 5
+# server_port = 1080
+#
+class Tsocks < Formula
+  url 'http://distfiles.macports.org/tsocks/tsocks-1.8.4.tar.bz2'
+  homepage 'http://tsocks.sourceforge.net/'
+  md5 'dccf57d632136b62dbe7ec7970b6c3e3'
+  def patches
+    {
+      :p0 => [
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-Makefile.in',
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-configure.in',
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-dead_pool.c',
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-tsocks.c',
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-tsocks.h',
+        'https://trac.macports.org/export/81420/trunk/dports/net/tsocks/files/patch-tsocks.in',
+      ]
+    }
+  end
+
+  def install
+    system "autoreconf"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+           "--prefix=#{prefix}", "--libdir=#{prefix}/lib", "--mandir=#{prefix}/share/man",
+           "--with-conf=#{etc}/tsocks.conf", "--enable-socksdns", "--disable-tordns"
+    system "make"
+    system "make install"
+  end
+end
